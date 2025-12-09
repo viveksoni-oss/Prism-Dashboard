6c0da813d39ba4acb788116bdd436d36ce0d8a96
Vivek Soni <vivek.soni@iitkfirst.com>
2025-12-09 12:35:06 +0530

    add shadcn ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+
 import "./App.css";
+import { Button } from "./components/ui/button";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <h1>Prism Dashboard</h1>{" "}
+      <h1 className="text-2xl">Prism Dashboard</h1>
+      <Button variant={"destructive"}>Enter</Button>
     </>
   );
 }
