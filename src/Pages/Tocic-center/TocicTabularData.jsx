664e1ce65e1920c21c5b0ed26ca7bdbf4766927f
Krishlay <krishlay@iitkfirst.com>
2025-12-11 17:59:16 +0530

    feat: update Tocic Center page with detailed invoice data and improve layout

--- src/Pages/Tocic-center/TocicTabularData.jsx
+++ src/Pages/Tocic-center/TocicTabularData.jsx
@@ -11,46 +11,70 @@ import {
 
 const invoices = [
   {
-    invoice: "INV001",
-    paymentStatus: "Paid",
-    totalAmount: "$250.00",
-    paymentMethod: "Credit Card",
+    TOCICCenterName: "SPMVV, Tirupati",
+    CenterHead: "XYZ",
+    ContactDetails: "98xxxxxx01",
+    Location: "INDIA",
   },
   {
-    invoice: "INV002",
-    paymentStatus: "Pending",
-    totalAmount: "$150.00",
-    paymentMethod: "PayPal",
+    TOCICCenterName: "CTAE Udaipur",
+    CenterHead: "XYZ",
+    ContactDetails: "98xxxxxx01",
+    Location: "INDIA",
   },
   {
-    invoice: "INV003",
-    paymentStatus: "Unpaid",
-    totalAmount: "$350.00",
-    paymentMethod: "Bank Transfer",
+    TOCICCenterName: "University Of Madras",
+    CenterHead: "XYZ",
+    ContactDetails: "98xxxxxx01",
+    Location: "INDIA",
   },
   {
-    invoice: "INV004",
-    paymentStatus: "Paid",
-    totalAmount: "$450.00",
-    paymentMethod: "Credit Card",
+    TOCICCenterName: "GSBTM Gandhinagar",
+    CenterHead: "XYZ",
+    ContactDetails: "98xxxxxx01",
+    Location: "INDIA",
   },
   {
-    invoice: "INV005",
-    paymentStatus: "Paid",
-    totalAmount: "$550.00",
-    paymentMethod: "PayPal",
+    TOCICCenterName: "CSIR-CMERI Durgapur",
+    CenterHead: "XYZ",
+    ContactDetails: "98xxxxxx01",
+    Location: "INDIA",
   },
   {
-    invoice: "INV006",
-    paymentStatus: "Pending",
-    totalAmount: "$200.00",
-    paymentMethod: "Bank Transfer",
+    TOCICCenterName: "IIT Kanpur",
+    CenterHead: "XYZ",
+    ContactDetails: "98xxxxxx01",
+    Location: "INDIA",
   },
   {
-    invoice: "INV007",
-    paymentStatus: "Unpaid",
-    totalAmount: "$300.00",
-    paymentMethod: "Credit Card",
+    TOCICCenterName: "NAL Banglore",
+    CenterHead: "XYZ",
+    ContactDetails: "98xxxxxx01",
+    Location: "INDIA",
+  },
+  {
+    TOCICCenterName: "NEIST Jorhat",
+    CenterHead:  "XYZ",
+    ContactDetails: "98xxxxxx01",
+    Location: "INDIA",
+  },
+  {
+    TOCICCenterName: "Gujrat",
+    CenterHead:  "XYZ",
+    ContactDetails: "98xxxxxx01",
+    Location: "INDIA",
+  },
+  {
+    TOCICCenterName: "CSIR-CGCRI",
+    CenterHead:  "XYZ",
+    ContactDetails: "98xxxxxx01",
+    Location: "INDIA",
+  },
+  {
+    TOCICCenterName: "IIT Guwahati",
+    CenterHead: "XYZ",
+    ContactDetails: "98xxxxxx01",
+    Location: "INDIA",
   },
 ]
 
@@ -60,19 +84,19 @@ export function TableDemo() {
       <TableCaption>A list of your recent invoices.</TableCaption>
       <TableHeader>
         <TableRow>
-          <TableHead className="w-[100px]">Invoice</TableHead>
-          <TableHead>Status</TableHead>
-          <TableHead>Method</TableHead>
-          <TableHead className="text-right">Amount</TableHead>
+          <TableHead className="w-[100px]">TOCIC CENTER</TableHead>
+          <TableHead>CenterHead</TableHead>
+          <TableHead>Contact Details</TableHead>
+          <TableHead className="text-right">Location</TableHead>
         </TableRow>
       </TableHeader>
       <TableBody>
         {invoices.map((invoice) => (
           <TableRow key={invoice.invoice}>
-            <TableCell className="font-medium">{invoice.invoice}</TableCell>
-            <TableCell>{invoice.paymentStatus}</TableCell>
-            <TableCell>{invoice.paymentMethod}</TableCell>
-            <TableCell className="text-right">{invoice.totalAmount}</TableCell>
+            <TableCell className="font-medium">{invoice.TOCICCenterName}</TableCell>
+            <TableCell>{invoice.CenterHead}</TableCell>
+            <TableCell>{invoice.ContactDetails}</TableCell>
+            <TableCell className="text-right">{invoice.Location}</TableCell>
           </TableRow>
         ))}
       </TableBody>
