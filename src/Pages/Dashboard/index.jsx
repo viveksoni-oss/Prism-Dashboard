75fe3c997d3bfa79decb3a2d79c0178eab1a9203
Vivek Soni <vivek.soni@iitkfirst.com>
2025-12-11 16:57:15 +0530

    feat: enhance Dashboard layout with multiple SpotlightCards and add ChartBarMultiple component

--- src/Pages/Dashboard/index.jsx
+++ src/Pages/Dashboard/index.jsx
@@ -2,18 +2,27 @@ import AppPieChart from "@/components/charts/pieChart";
 import React from "react";
 
 import SpotlightCard from "./../../components/SpotlightCard";
+import { ChartBarMultiple } from "@/components/charts/barChart";
 
 function Dashboard() {
   return (
-    <div className="text-2xl">
-      hello
-      <SpotlightCard
-        className="custom-spotlight-card"
-        spotlightColor="rgba(0, 229, 255, 0.2)"
-      >
-        // Content goes here
-      </SpotlightCard>
-      <AppPieChart></AppPieChart>
+    <div className="text-2xl p-10">
+      <div className=" grid grid-cols-12 gap-10">
+        {Array.from({ length: 4 }, () => (
+          <SpotlightCard
+            className="custom-spotlight-card col-span-4"
+            spotlightColor="rgba(0, 229, 255, 0.2)"
+          >
+            <AppPieChart></AppPieChart>
+          </SpotlightCard>
+        ))}
+        <SpotlightCard
+          className="custom-spotlight-card col-span-8"
+          spotlightColor="rgba(0, 229, 255, 0.2)"
+        >
+          <ChartBarMultiple></ChartBarMultiple>
+        </SpotlightCard>
+      </div>
     </div>
   );
 }
